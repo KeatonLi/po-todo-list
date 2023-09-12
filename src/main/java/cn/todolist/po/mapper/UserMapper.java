63af76b6401699ad21b2095a0559147b8f381cb2
libokai <li_bo_kai@qq.com>
2023-09-12 19:15:24 +0800

    feat：todo任务和用户表mapper构建

---
+++ src/main/java/cn/todolist/po/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package cn.todolist.po.mapper;
+
+import cn.todolist.po.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
