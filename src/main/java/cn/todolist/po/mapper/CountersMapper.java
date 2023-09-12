693ec38f03319bedcd32cb2fb21778ca522c03b5
libokai <li_bo_kai@qq.com>
2023-09-12 15:06:20 +0800

    feat：引入mybatis-plus

--- src/main/java/cn/todolist/po/mapper/CountersMapper.java
+++ src/main/java/cn/todolist/po/mapper/CountersMapper.java
@@ -1,11 +1,12 @@
 package cn.todolist.po.mapper;
 
 import cn.todolist.po.model.Counter;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
-public interface CountersMapper {
+public interface CountersMapper extends BaseMapper<Counter> {
 
     Counter getCounter(@Param("id") Integer id);
 
