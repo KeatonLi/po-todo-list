68333d66811f71263a67209b58094c7049f8620c
黎博凯 <m13922853274@163.com>
2023-09-17 21:07:57 +0800

    feat：MD5工具类优化

--- src/main/java/cn/todolist/po/utils/MD5Utils.java
+++ src/main/java/cn/todolist/po/utils/MD5Utils.java
@@ -3,23 +3,26 @@ package cn.todolist.po.utils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Slf4j
 public class MD5Utils {
+
+    static MessageDigest md5;
+
+    static {
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     /***
      * MD5加码 生成32位md5码
      */
     public static String string2MD5(String inStr) {
-        MessageDigest md5 = null;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (Exception e) {
-            log.info(e.toString());
-            e.printStackTrace();
-            return "";
-        }
         char[] charArray = inStr.toCharArray();
         byte[] byteArray = new byte[charArray.length];
 
@@ -37,8 +40,5 @@ public class MD5Utils {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(string2MD5("123456"));
-    }
 
 }
